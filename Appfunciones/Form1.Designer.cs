a40385b2f722764f053cca8e0ad2608de2f47344
jhourodriguez5701 <jhousaso@gmail.com>
2019-11-06 12:30:43 -0500

    funcion suma de enteros

--- Appfunciones/Form1.Designer.cs
+++ Appfunciones/Form1.Designer.cs
@@ -28,34 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.btmsuamar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btncerrar
             // 
-            this.button1.Location = new System.Drawing.Point(120, 100);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "SUMAR";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btncerrar.Location = new System.Drawing.Point(210, 242);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(75, 23);
+            this.btncerrar.TabIndex = 0;
+            this.btncerrar.Text = "CERRAR";
+            this.btncerrar.UseVisualStyleBackColor = true;
+            this.btncerrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese un entero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese otro entero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado";
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(185, 55);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(100, 20);
+            this.txtnum1.TabIndex = 4;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(185, 173);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(100, 20);
+            this.txtresultado.TabIndex = 5;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(185, 116);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(100, 20);
+            this.txtnum2.TabIndex = 6;
+            // 
+            // btmsuamar
+            // 
+            this.btmsuamar.Location = new System.Drawing.Point(103, 242);
+            this.btmsuamar.Name = "btmsuamar";
+            this.btmsuamar.Size = new System.Drawing.Size(75, 23);
+            this.btmsuamar.TabIndex = 7;
+            this.btmsuamar.Text = "SUMAR";
+            this.btmsuamar.UseVisualStyleBackColor = true;
+            this.btmsuamar.Click += new System.EventHandler(this.btmsuamar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(369, 361);
+            this.Controls.Add(this.btmsuamar);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncerrar);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.Button btmsuamar;
     }
 }
 
--- Appfunciones/Form1.cs
+++ Appfunciones/Form1.cs
@@ -19,10 +19,20 @@ namespace Appfunciones
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+            //cierra el formulario
+            this.Close();
         }
-        private int fnsuma(int a, int b)
+
+        private void btmsuamar_Click(object sender, EventArgs e)
         {
+            int num1 = Int32.Parse( this.txtnum1.Text.ToString());
+            int num2 = Int32.Parse(this.txtnum2.Text.ToString());
+            int suma = fnsuma(num1, num2);
+            //asigno resultado en cuadro de texto
+            this.txtresultado.Text = suma.ToString();
+        }
+        int fnsuma(int a, int b)
+            {
             return (a + b);
         }
     }
