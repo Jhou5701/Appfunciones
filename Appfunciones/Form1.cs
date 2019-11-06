3e39fbf22de075a5f5333a93e4917d8f7fe9e16e
jhourodriguez5701 <jhousaso@gmail.com>
2019-11-06 12:01:04 -0500

    funcion suma

--- Appfunciones/Form1.cs
+++ Appfunciones/Form1.cs
@@ -16,5 +16,14 @@ namespace Appfunciones
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private int fnsuma(int a, int b)
+        {
+            return (a + b);
+        }
     }
 }
