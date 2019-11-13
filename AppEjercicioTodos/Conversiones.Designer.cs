34d1494eefe20ea9142b5c8a538d6c38e5ecdb27
jhourodriguez5701 <jhousaso@gmail.com>
2019-11-13 12:43:44 -0500

    Ejercicio 2.14

---
+++ AppEjercicioTodos/Conversiones.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace AppEjercicioTodos
+{
+    partial class Conversiones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtmetros = new System.Windows.Forms.TextBox();
+            this.txtpulgadas = new System.Windows.Forms.TextBox();
+            this.txtpies = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(23, 150);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(121, 150);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnnuevo.TabIndex = 1;
+            this.btnnuevo.Text = "Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            this.btnnuevo.Click += new System.EventHandler(this.btnnuevo_Click);
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.Location = new System.Drawing.Point(223, 150);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(75, 23);
+            this.btncerrar.TabIndex = 2;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = true;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Metros";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pies";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Pulgadas";
+            // 
+            // txtmetros
+            // 
+            this.txtmetros.Location = new System.Drawing.Point(120, 38);
+            this.txtmetros.MaxLength = 10;
+            this.txtmetros.Name = "txtmetros";
+            this.txtmetros.Size = new System.Drawing.Size(100, 20);
+            this.txtmetros.TabIndex = 6;
+            // 
+            // txtpulgadas
+            // 
+            this.txtpulgadas.Enabled = false;
+            this.txtpulgadas.Location = new System.Drawing.Point(120, 88);
+            this.txtpulgadas.Name = "txtpulgadas";
+            this.txtpulgadas.Size = new System.Drawing.Size(100, 20);
+            this.txtpulgadas.TabIndex = 7;
+            // 
+            // txtpies
+            // 
+            this.txtpies.Enabled = false;
+            this.txtpies.Location = new System.Drawing.Point(120, 62);
+            this.txtpies.Name = "txtpies";
+            this.txtpies.Size = new System.Drawing.Size(100, 20);
+            this.txtpies.TabIndex = 8;
+            // 
+            // Conversiones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 186);
+            this.Controls.Add(this.txtpies);
+            this.Controls.Add(this.txtpulgadas);
+            this.Controls.Add(this.txtmetros);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btnnuevo);
+            this.Controls.Add(this.btncalcular);
+            this.Name = "Conversiones";
+            this.Text = "Conversiones";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtmetros;
+        private System.Windows.Forms.TextBox txtpulgadas;
+        private System.Windows.Forms.TextBox txtpies;
+    }
+}
---
+++ AppEjercicioTodos/Conversiones.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEjercicioTodos
+{
+    public partial class Conversiones : Form
+    {
+        public Conversiones()
+        {
+            InitializeComponent();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private double Fmetros(double metros)
+        {
+            return (metros * 39.37);
+
+        }
+        private double Fpies(double pies)
+        {
+            return (pies * 3.28084);
+
+        }
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            if (this.txtmetros.Text.Length==0)
+            {
+                MessageBox.Show("Debes ingresar los metros");
+                this.txtmetros.Focus();
+                return;
+            }
+            try
+            {
+                double metros = Double.Parse(this.txtmetros.Text);
+                this.txtpulgadas.Text = Fmetros(metros).ToString();
+                double pies = Double.Parse(this.txtmetros.Text);
+                this.txtpies.Text = Fpies(pies).ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            
+        }
+
+        private void btnnuevo_Click(object sender, EventArgs e)
+        {
+            this.txtmetros.Text = "";
+            this.txtpies.Text = "";
+            this.txtpulgadas.Text = "";
+
+        }
+    }
+}
